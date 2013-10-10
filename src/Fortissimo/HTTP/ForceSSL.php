3b39d80ec242950ec418d9f105d8943014d4c49b
Matt Butcher <technosophos@gmail.com>
2013-10-09 21:03:09 -0600

    Added a flag to enable/disable forceSsl.

--- src/Fortissimo/HTTP/ForceSSL.php
+++ src/Fortissimo/HTTP/ForceSSL.php
@@ -6,10 +6,14 @@ class ForceSSL extends \Fortissimo\Command\Base {
   public function expects() {
     return $this
       ->description("Force the client to use SSL to access this route.")
+      ->usesParam('force', 'If this is true, the redirect is forced. Otherwise, we silently continue.')->whichHasDefault(TRUE)
       ->andReturns("Nothing. Issues a 301 redirect.")
       ;
   }
   public function doCommand() {
+    if ($this->param('force') === TRUE) {
+      return;
+    }
     if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == 'off') {
       $host = empty($_SERVER['HTTP_HOST']) ? $_SERVER['SERVER_NAME'] : $_SERVER['HTTP_HOST'];
       $redirect = sprintf(self::tpl, $host, $_SERVER['REQUEST_URI']);
