da5e0f38197965291294fc1656c3b0351577d3da
Matt Butcher <technosophos@gmail.com>
2013-10-14 15:39:34 -0600

    Added a prefix parameter.

--- src/Fortissimo/Commons/FromArray.php
+++ src/Fortissimo/Commons/FromArray.php
@@ -6,7 +6,9 @@ namespace Fortissimo\Commons;
  * This allows you to take an associative or indexed array and put the values into the context.
  *
  * Params:
- * - Any name becomes a key, and its value is put into the context with the name as the key.
+ * - array: The array of data to use as a source.
+ * - keys: The keys that should be extracted from `array`.
+ * - prefix: If set, this string will be prepended to each key before the key is put into context.
  *
  * Example:
  * ->does('FromArray', 'myData')
@@ -21,6 +23,8 @@ namespace Fortissimo\Commons;
  *
  * The 'another' key is not extracted into the context.
  *
+ * The 'prefix' parameter can be used to avoid context collisions with existing names.
+ *
  */
 class FromArray extends \Fortissimo\Command\Base {
   public function expects() {
@@ -28,16 +32,18 @@ class FromArray extends \Fortissimo\Command\Base {
       ->description('Extract data from an array into the context.')
       ->usesParam('array', 'The array to extract from.')->whichIsRequired()
       ->usesParam('keys', 'The keys. These can be numeric or string')->whichIsRequired()
+      ->usesParam('prefix', 'An optional prefix that will be added to the key name before the keyname is put into context.')
       ->andReturns('Each key is inserted into the context with the value found in the array (or NULL if none is found).')
       ;
   }
   public function doCommand() {
     $array = $this->param('array');
     $keys = $this->param('keys');
+    $prefix = $this->param('prefix', '');
 
     foreach ($keys as $key) {
       $val = isset($array[$key]) ? $array[$key] : NULL;
-      $this->context->add($key, $val);
+      $this->context->add($prefix . $key, $val);
     }
   }
 }
